3ca3a3abb42943699b344093eaf3acd2f02bc562
ramonmelod <ramonmelo.com@gmail.com>
2024-10-04 16:09:41 -0300

    feat: implementation of the scrolling with the two arrow with the hidden overflow

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,33 @@
 
 const imagesContainer = document.querySelector('.images');
+const image01 = document.querySelector('#image-01');
+const image02 = document.querySelector('#image-02');
+const image03 = document.querySelector('#image-03');
+const image04 = document.querySelector('#image-04');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const logo = document.querySelector('.logo');
 let scrollAmount = 0;
-const scrollStep = 270; 
+const scrollStep = 50; 
+
+
 
 prevBtn.addEventListener('click', () => {
-  //logo.style.left = "500px";
-  //scrollAmount = Math.max(scrollAmount - scrollStep, 0);
-  imagesContainer.style.transform = `translateX(-50px)`; //${scrollAmount}
-});
+  scrollAmount = scrollAmount + scrollStep;
+  image01.style.transform = `translateX(${scrollAmount}px)`
+  image02.style.transform = `translateX(${scrollAmount}px)`
+  image03.style.transform = `translateX(${scrollAmount}px)`
+  image04.style.transform = `translateX(${scrollAmount}px)`
 
-// nextBtn.addEventListener('click', () => {
-//   const maxScroll = imagesContainer.scrollWidth - imagesContainer.clientWidth;
-//   scrollAmount = Math.min(scrollAmount + scrollStep, maxScroll);
-//   imagesContainer.style.transform = `translateX(-${scrollAmount}px)`;
-// });
+  // //logo.style.left = "500px";
+  // scrollAmount = scrollAmount + scrollStep;
+  // console.log(scrollAmount)
+  // imagesContainer.style.transform = `translateX(${scrollAmount}px)`; //
+});
+nextBtn.addEventListener('click', () => {
+  scrollAmount = scrollAmount - scrollStep;
+  image01.style.transform = `translateX(${scrollAmount}px)`
+  image02.style.transform = `translateX(${scrollAmount}px)`
+  image03.style.transform = `translateX(${scrollAmount}px)`
+  image04.style.transform = `translateX(${scrollAmount}px)`
+});
