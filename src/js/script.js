757234213fbacee62cbe17aad7d3e7a84478f84b
Ramonmelod <ramonmelo.com@gmail.com>
2024-10-07 14:43:18 -0300

    feat: images horizontal movement with scrollBy method implementation

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,11 @@
-
-const imagesContainer = document.querySelector('.images');
-const image01 = document.querySelector('#image-01');
-const image02 = document.querySelector('#image-02');
-const image03 = document.querySelector('#image-03');
-const image04 = document.querySelector('#image-04');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const logo = document.querySelector('.logo');
+const imagesContainer = document.querySelector(".images");
+const logo = document.querySelector(".logo");
 let scrollAmount = 0;
-const scrollStep = 270; 
+const scrollStep = 270;
 
-
-
-prevBtn.addEventListener('click', () => {
-  scrollAmount = scrollAmount + scrollStep;
-  image01.style.transform = `translateX(${scrollAmount}px)`
-  image02.style.transform = `translateX(${scrollAmount}px)`
-  image03.style.transform = `translateX(${scrollAmount}px)`
-  image04.style.transform = `translateX(${scrollAmount}px)`
-
-  //imagesContainer.scrollBy(500,0)
-
-
+prevBtn.addEventListener("click", () => {
+  imagesContainer.scrollBy(-350, 0);
 });
-nextBtn.addEventListener('click', () => {
-  scrollAmount = scrollAmount - scrollStep;
-  image01.style.transform = `translateX(${scrollAmount}px)`
-  image02.style.transform = `translateX(${scrollAmount}px)`
-  image03.style.transform = `translateX(${scrollAmount}px)`
-  image04.style.transform = `translateX(${scrollAmount}px)`
+nextBtn.addEventListener("click", () => {
+  imagesContainer.scrollBy(350, 0);
 });
