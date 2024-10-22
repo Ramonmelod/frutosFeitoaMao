ca57fe78dd74c16ada692c9ff56fd0c9be3679ed
Ramonmelod <ramonmelo.com@gmail.com>
2024-10-22 09:07:11 -0300

    refactor: creating the file scroll.js to split the scroll logic from the script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+import { scroll } from "./scroll.js";
+
 const imagesContainer = document.querySelector(".images");
 const menuIcon = document.getElementById("menu-icon");
 const navMenu = document.getElementById("nav-menu");
@@ -83,33 +85,4 @@ const queryJson = async () => {
 
 queryJson(); //calls the function
 
-//-------begining of the scroll logic
-
-let scrollAmount = 0;
-const scrollStep = 270;
-
-prevBtn.disabled = false; //declare the prevBtn.disabled for use in disabling feature
-nextBtn.disabled = false;
-
-prevBtn.addEventListener("click", () => {
-  //left
-  imagesContainer.scrollBy(-321, 0);
-  prevBtn.disabled = true; //disable the prevBtn for avoiding fast double click to cause the picture scrolling to desalign
-  nextBtn.disabled = true;
-  setTimeout(() => {
-    prevBtn.disabled = false; //reenable the prevBtn after 250ms
-    nextBtn.disabled = false;
-  }, 250);
-});
-nextBtn.addEventListener("click", () => {
-  //right
-  prevBtn.disabled = true;
-  nextBtn.disabled = true; //disable the nextBtn for avoiding fast double click to cause the picture scrolling to desalign
-  imagesContainer.scrollBy(321, 0);
-  setTimeout(() => {
-    prevBtn.disabled = false;
-    nextBtn.disabled = false; //reenable the nextBtn after 250ms
-  }, 250);
-});
-
-//-------end of the scroll logic
+scroll(imagesContainer);
---
+++ src/js/scroll.js
@@ -0,0 +1,27 @@
+export const scroll = (imagesContainer) => {
+  const scrollStep = 321;
+
+  prevBtn.disabled = false; //declare the prevBtn.disabled for use in disabling feature
+  nextBtn.disabled = false;
+
+  prevBtn.addEventListener("click", () => {
+    //left
+    imagesContainer.scrollBy(-scrollStep, 0);
+    prevBtn.disabled = true; //disable the prevBtn for avoiding fast double click to cause the picture scrolling to desalign
+    nextBtn.disabled = true;
+    setTimeout(() => {
+      prevBtn.disabled = false; //reenable the prevBtn after 250ms
+      nextBtn.disabled = false;
+    }, 250);
+  });
+  nextBtn.addEventListener("click", () => {
+    //right
+    prevBtn.disabled = true;
+    nextBtn.disabled = true; //disable the nextBtn for avoiding fast double click to cause the picture scrolling to desalign
+    imagesContainer.scrollBy(scrollStep, 0);
+    setTimeout(() => {
+      prevBtn.disabled = false;
+      nextBtn.disabled = false; //reenable the nextBtn after 250ms
+    }, 250);
+  });
+};
