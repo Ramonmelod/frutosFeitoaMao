82bf6499efacc2ce49547b768206f76f82b05d46
ramonmelod <ramonmelo.com@gmail.com>
2024-10-24 20:24:49 -0300

    fix: erasing the value inside the searchBar and text of the reseachResult after the research being made

--- src/js/searchBar.js
+++ src/js/searchBar.js
@@ -5,6 +5,7 @@ const logo = document.querySelector(".logo");
 const slogan = document.querySelector(".slogan");
 const txtIntro = document.querySelector(".txt_intro");
 const resultContainer = document.getElementById("searchResults");
+const searchBar = document.getElementById("searchInput"); //declaration of the searchbar
 
 export const queryDoc = async () => {
   try {
@@ -14,9 +15,7 @@ export const queryDoc = async () => {
       .addEventListener("submit", function (e) {
         e.preventDefault(); // Prevents the default behavior of sending the forms
         removeProductDisplay(); //remove the current displayed products
-        const query = document // get the value in the bar and change it all to lower case
-          .getElementById("searchInput")
-          .value.toLowerCase();
+        const query = searchBar.value.toLowerCase(); // get the value in the bar and change it all to lower case
 
         //-------------------------------------------------------------------------
         const filteredProducts = data.filter((product) =>
@@ -25,13 +24,14 @@ export const queryDoc = async () => {
 
         if (filteredProducts.length > 0) {
           filteredProducts.forEach((product) => {
+            searchBar.value = ""; // erase the value inside the searchbar
+            resultContainer.textContent = ""; // erases the sentece "Nenhum produto encontrado."
             logo.insertAdjacentElement("afterend", slogan); //appending slogan if it is not present
             logo.insertAdjacentElement("afterend", txtIntro); //appending txt_intro if it is not present
             productDisplay(product.id); // here the elements reaseached in the bar  are created and shown
           });
         } else {
-          console.log("agora1");
-          // logo.remove();
+          searchBar.value = ""; // erase the value inside the searchbar
           slogan.remove(); //remove the slogan
           txtIntro.remove(); //remove the txt_intro
           resultContainer.textContent = "Nenhum produto encontrado.";
