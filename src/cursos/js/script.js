cdf0dbe9d3ad27c49b71960b40db39e8e68ba9b0
Ramon Melo <contato@ramonmelo.com.br>
2025-11-05 15:38:35 -0300

    feat: initial implementation of the curse card

---
+++ src/cursos/js/script.js
@@ -0,0 +1,25 @@
+import { productDisplay } from "./productDetails.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    //makes this scope waits until the DOM be totally loaded
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get("productId");
+    const imageIndex = params.get("imageIndex");
+
+    if (productId > 25) {
+      /* const productDetailContainer = document.querySelector(".productDetail");
+      const notFound = document.createElement("p");
+      notFound.textContent = "Page not Found!";
+      productDetailContainer.appendChild(notFound);
+      productDetailContainer.style.Top = "50%";
+      productDetailContainer.style.color = "#000";*/
+      throw new Error("Page not Found");
+    }
+    productDisplay(productId, imageIndex);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//http://127.0.0.1:5500/src/productDetails/index.html?productId=9
