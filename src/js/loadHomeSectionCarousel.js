c0a057df2dd9d01a0ca6e11a00b9a3726b7bc7b1
Ramon Melo <ramonmelo.com@gmail.com>
2025-12-15 11:39:27 -0300

    style: reduce the margin-top of the section  /index.html in mobile mode

--- src/js/loadHomeSectionCarousel.js
+++ src/js/loadHomeSectionCarousel.js
@@ -13,13 +13,13 @@ loadSectionCarousel({
     {
       title: "Cursos",
       image:
-        "https://res.cloudinary.com/dhayneykx/image/upload/v1762440042/IMG-20251106-WA0028_mjawzm.jpg",
+        "https://res.cloudinary.com/dhayneykx/image/upload/v1762440042/IMG-20251106-WA0024_lxrrzr.jpg",
       link: "/cursos/index.html",
     },
     {
       title: "Riscos para Bordar",
       image:
-        "https://res.cloudinary.com/dhayneykx/image/upload/v1729042399/frutosfeitoamao/p4woczkqoij1ufrxp88k.jpg",
+        "https://res.cloudinary.com/dhayneykx/image/upload/v1765797232/1e21f4f8-ce6e-4b4b-83e0-b382e7c94ed0_tqynjl.jpg",
       link: "/riscos-para-bordar/index.html",
     },
     {
