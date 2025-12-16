8f545b8e19f414537e17ac629017f505223e4136
ramonmelod <ramonmelo.com@gmail.com>
2025-12-16 10:22:47 -0300

    add the `bordados` page to the site

---
+++ src/bordados/bordadoDetailsScript.js
@@ -0,0 +1,29 @@
+import { productDisplay } from "/js/productDetails.js";
+
+const coursesPath = "/productCards.json";
+const buyPageLink =
+  "https://wa.me/5548991035724?text=Ol%C3%A1%2C+venho+atrav%C3%A9s+do+site+frutosfeitoamao.com.br+e+me+interessei+pelo+produto"; //"/pagamentos/index.html"; //change for using the api
+const buyButtonText = "Solicitar Orçamento";
+const thumbNailLink = "./bordadoDetails.html?productId";
+
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    //makes this scope waits until the DOM be totally loaded
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get("productId");
+    const imageIndex = params.get("imageIndex");
+
+    //NEED TO MAKE A PAGE NOT FOUND WHEN THE PRODUCT ID IS NOT IN THE COURSESCARD.JSON
+    productDisplay(
+      true, // when the the true option is enabled the buyPageLink goes to whatsapp
+      coursesPath,
+      productId,
+      imageIndex,
+      buyPageLink,
+      buyButtonText,
+      thumbNailLink
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
