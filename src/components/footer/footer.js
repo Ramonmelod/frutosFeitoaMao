7b03d7a1c098c46e184344fed31db644bd6fe66d
Ramon Melo <ramonmelo.com@gmail.com>
2025-12-27 10:31:42 -0300

    feat: implements the injection of the current year in the footer copyright text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,10 @@ async function loadFooter() {
     const res = await fetch("/components/footer/footer.html");
     const html = await res.text();
     document.getElementById("footer-root").innerHTML = html;
+
+    const year = new Date().getFullYear();
+    const copyRightText = document.querySelector(".copyright");
+    copyRightText.innerHTML = `© ${year} Frutos Feito à Mão`; // insert the current year in the copyright text
   } catch (error) {
     console.log(error);
     throw error;
